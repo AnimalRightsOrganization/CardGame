18c411cc32352fca41eb091773c07917df5a144d
Setsuodu <339131001@qq.com>
2017-03-07 02:40:33 +0800

    Update Lesson 5
    Card enum init.

--- Assets/Game/Scripts/Tools/Consts.cs
+++ Assets/Game/Scripts/Tools/Consts.cs
@@ -16,7 +16,82 @@ public class Consts
 /// <summary>
 /// 面板类型
 /// </summary>
-public enum PanelType
+public enum paneltype
 {
 
 }
+
+//summary可以再鼠标悬停时显示提示。
+/// <summary> 
+/// 角色类型
+/// </summary>
+public enum CharacterType
+{
+    Library = 0, //牌库
+    Player = 1, //玩家
+    ComputerRight = 2, //电脑1
+    ComputerLeft = 3, //电脑2
+    Desk = 4 //桌子
+}
+
+/// <summary>
+/// 卡牌花色
+/// </summary>
+public enum Colors
+{
+    King, //王
+    Club, //梅花
+    Heart, //红桃
+    Spade, //黑桃
+    Square //方片
+}
+
+/// <summary>
+/// 卡牌权值 2,3,4,5,6,7,8,9,10,J,Q,K,A,2,SmallJoker,BigJoker
+/// </summary>
+public enum Weight
+{
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    One,
+    Two,
+    SJoker,
+    BJoker
+}
+
+/// <summary>
+/// 出牌类型
+/// </summary>
+public enum CardType
+{
+    None,
+    Single, //单张
+    Double, //对子
+    Straight1, //顺子
+    Straight2, //双顺子
+    Straight3, //三顺子
+    Three, //三张不带
+    ThreeAndOne, //三带一
+    ThreeAndTwo, //三带二
+    Boom, //炸弹
+    JokerBoom //王炸弹
+}
+
+/// <summary>
+/// 身份
+/// </summary>
+public enum Identity
+{
+    Farmer, //农民
+    LandLord //地主
+}
+
