d65645cad4c0168d40c920a3fd18c49482825c28
setsuodu <339131001@qq.com>
2021-10-05 16:07:31 +0800

    通过名称获取贴图

---
+++ Assets/TestCard.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestCard : MonoBehaviour
+{
+    public Image card;
+
+    public CardAttribute CardAttribute;
+
+    void Start()
+    {
+        Loadcardfrontatlasnamed();
+    }
+
+    static Dictionary<string, Sprite> atlasCardfront = new Dictionary<string, Sprite>();
+    static void Loadcardfrontatlasnamed()
+    {
+        Sprite[] sprites = Resources.LoadAll<Sprite>($"Textures/sprite");
+        atlasCardfront.Clear();
+        foreach (Sprite asprite in sprites)
+        {
+            atlasCardfront[asprite.name] = asprite;
+        }
+        Debug.Log(atlasCardfront.Count);
+    }
+
+    [ContextMenu("ChangeCard")]
+    void Change()
+    {
+        string card_name = $"{CardAttribute.colors}_{(int)CardAttribute.weight}";
+        Debug.Log(card_name);
+        var sp = atlasCardfront[card_name];
+        card.sprite = sp;
+    }
+}
